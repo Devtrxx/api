c58c656700bfcfd103c54cae719d34b5ce6de489
devesh mishra <112480549+devesh-mishra13@users.noreply.github.com>
2024-02-03 12:42:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,27 +10,29 @@
 
 # @app.post("/calculate_distance")
 # def calculate_distance(data: dict):
-#     car_type = data['cartype']
-#     car = np.array(car_type)
-#     main = label.transform(car)
+    # print("hello world!")
+    # car_type = data['carType']
+    # car = np.array([car_type])
+    # print(car,car.shape)
+    # main = label.transform(car)
 
-#     try:
-#         km = response["choices"][0]["message"]["content"]
-#         km = ((int)(km[:-2]))*1000
-#         kilo = np.array(km)
+    # try:
+    #     km = "20 km"
+    #     km = ((int)(km[:-2]))*1000
+    #     kilo = np.array(km)
 
-#         X_train = np.c_[kilo,main].reshape(-1,1)
-#         X_norm = scaler.transform(X_train)
-#         return model.predict(X_norm)
+    #     X_train = np.c_[kilo,main].reshape(-1,1)
+    #     X_norm = scaler.transform(X_train)
+    #     return model.predict(X_norm)
 
 
 
-#     except Exception as e:
-#         print(f"Error generating distance: {str(e)}")
+    # except Exception as e:
+    #     print(f"Error generating distance: {str(e)}")
 from fastapi import FastAPI, HTTPException
 from joblib import load
 import numpy as np
-import httpx 
+import httpx
 import requests
 
 app = FastAPI()
@@ -39,35 +41,33 @@ model = load('sgd_regressor_model.joblib')
 label = load('label_encoder.joblib')
 scaler = load('scaler.joblib')
 
-distance_from_maps = "http://127.0.0.1:8001/distance_from_maps"
+distance_from_maps = "http://192.168.50.67:8000/distance"
 
 @app.post("/calculate_distance")
-def calculate_distance(data: dict):
+async def calculate_distance(data: dict):
+    print(data)
     car_type = data['carType']
+    car = np.array([car_type])
+    main = label.transform(car)
 
 
-    main = label.transform(np.array(car_type).reshape(-1, 1))
-
-
-    data = {
-        "from": data["from"], 
-        "to": data["to"],  
-    }
-
-    response = requests.post(distance_from_maps, json=data)
+    response = httpx.post(distance_from_maps, json=data)
     km = response.json()
-
+    print(km)
     try:
-        km = ((int)(km)) * 1000
-        kilo = np.array(km)
+        km = ((int)(km))
+        kilo = np.array([km])
+        print(main,main.shape,kilo.shape)
 
-        X_train = np.c_[kilo, main].reshape(-1, 2)
+        X_train = np.c_[kilo, main].reshape(-1,2)
+        print(X_train.shape)
         X_norm = scaler.transform(X_train)
+        print(X_norm.shape)
 
 
         prediction = model.predict(X_norm)
 
-        return {"distance_prediction": prediction.item(), "km_from_openai": km}
+        return {'og':str(round(prediction[0]))+"Rs",'distance':str(round(km/1000))+"km"}
 
     except Exception as e:
         print(f"Error generating distance: {str(e)}")
